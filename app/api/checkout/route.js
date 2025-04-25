19747c324bc2bbbb14e1ad57dab4203e7a9f5e1b
Osamaas04 <alasmarosama7@gmail.com>
2025-04-26 01:22:11 +0300

    subscription 5

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -5,8 +5,8 @@ import { NextResponse } from "next/server";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request) {
-    const { priceId, user_id } = await request.json();
-    // const user_id = getUserIdFromToken(request);
+    const { priceId } = await request.json();
+    const user_id = getUserIdFromToken(request);
   
     const session = await stripe.checkout.sessions.create({
       line_items: [
@@ -20,7 +20,7 @@ export async function POST(request) {
         user_id 
       },
       success_url: `${process.env.NEXT_PUBLIC_APP_URL}/dashboard`,
-      cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/cancel`,
+      cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}`,
     });
   
     return NextResponse.json({ url: session.url });
