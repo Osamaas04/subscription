900ca1faa7868b1241445c5c91b7094e8ec35dcf
Osamaas04 <alasmarosama7@gmail.com>
2025-04-25 18:17:42 +0300

    subscription 4

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -1,5 +1,6 @@
 import { getUserIdFromToken } from "@/utils/getUserIdFromToken";
 import Stripe from "stripe";
+import { NextResponse } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
