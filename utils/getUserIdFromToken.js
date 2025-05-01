d6de9b079637115988823f0c851610ebb14f0104
Osamaas04 <alasmarosama7@gmail.com>
2025-05-01 20:29:04 +0300

    stripe 11

--- utils/getUserIdFromToken.js
+++ utils/getUserIdFromToken.js
@@ -1,11 +1,16 @@
 export const getUserIdFromToken = (request) => {
 
-    const user_id = request.headers.get("x-user-id")
-  
-    if (!user_id) {
-      throw new Error('Unauthorized: No user id');
-    }
-  
-    return user_id;
+  const user_id = request.headers.get("x-user-id")
+
+  const email = request.headers.get("Authorization")?.value;
+
+  console.log(email)
+
+  if (!user_id) {
+    throw new Error('Unauthorized: No user id');
+  }
+
+  return {
+    user_id
   };
-  
+};
